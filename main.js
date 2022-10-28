531f4922eef16021073af3dc7eb5465070592283
Huseyin <huseyin@example.com>
2022-10-28 12:20:43 +0300

    Multiple clocks

--- main.js
+++ main.js
@@ -1,58 +1,92 @@
-const canvas = document.querySelector('canvas#clock');
+const clock = (selector, timezone = 'Europe/Istanbul') => {
+  const canvas = selector;
 
-const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
 
-const offsetX = 50;
-const offsetY = 50;
-const size = 100;
+  const offsetX = 50;
+  const offsetY = 50;
+  const size = 100;
 
-const fps = 1;
+  const fps = 1;
 
-const getDim = (trigFunc, expr, factor) => trigFunc(
-    expr * 2 * Math.PI - (Math.PI / 2)
-  ) * size * factor + size;
+  const ONE_SECOND = 1000;
+  const ONE_MINUTE = ONE_SECOND * 60;
+  const ONE_HOUR = ONE_MINUTE * 60;
 
-const getX = (expr, factor) => getDim(Math.cos, expr, factor);
-const getY = (expr, factor) => getDim(Math.sin, expr, factor);
+  const cityName = timezone.split('/').at(-1);
 
-const drawHand = (ctx, expr, length) => {
-    ctx.beginPath();
-    ctx.moveTo(offsetX + size, offsetY + size);
-    ctx.lineTo(offsetX + getX(expr, length), offsetY + getY(expr, length));
-    ctx.stroke();
-}
+  const getDim = (trigFunc, expr, factor) => trigFunc(
+      expr * 2 * Math.PI - (Math.PI / 2)
+    ) * size * factor + size;
 
-ctx.font = 'bold 20px sans-serif';
-ctx.textAlign = 'center';
-ctx.textBaseline = 'middle';
-ctx.lineWidth = 6;
-ctx.lineCap = 'round';
+  const getX = (expr, factor) => getDim(Math.cos, expr, factor);
+  const getY = (expr, factor) => getDim(Math.sin, expr, factor);
 
-const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (let i = 1; i <= 12; i++) {
-    ctx.fillText(
-      i, 
-      offsetX + getX(i / 12, 1), 
-      offsetY + getY(i / 12, 1)
-    );
+  const drawHand = (ctx, expr, length) => {
+      ctx.beginPath();
+      ctx.moveTo(offsetX + size, offsetY + size);
+      ctx.lineTo(offsetX + getX(expr, length), offsetY + getY(expr, length));
+      ctx.stroke();
   }
-  
-  const date = new Date();
 
-  const [ h, m, s ] = [ 
-    date.getHours() % 12, date.getMinutes(), date.getSeconds() 
-  ];
+  let date = null;
 
-  drawHand(ctx, h / 12, 0.4);
-  drawHand(ctx, m / 60, 0.6);
-  drawHand(ctx, s / 60, 0.8);
-  
-  setTimeout(() => {
+  let [ h, m, s ] = [ 0, 0, 0 ];
+
+  const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = 'bold 20px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.lineWidth = 6;
+    ctx.lineCap = 'round';
+
+    for (let i = 1; i <= 12; i++) {
+      ctx.fillText(
+        i, 
+        offsetX + getX(i / 12, 1), 
+        offsetY + getY(i / 12, 1)
+      );
+    }
+    
+    drawHand(ctx, (h % 12) / 12, 0.5);
+    drawHand(ctx, m / 60, 0.6);
+    drawHand(ctx, s / 60, 0.8);
+
+    ctx.font = 'normal 20px monospace';
+    ctx.textBaseline = 'top';
+
+    const pad = val => val.toString().padStart(2, 0);
+
+    const digitalTime = `${pad(h)}:${pad(m)}:${pad(s)}`;
+    
+    ctx.fillText(digitalTime, offsetX + size, offsetY - 35);
+
+    ctx.font = 'normal 30px monospace';
+    
+    ctx.fillText(cityName, offsetX + size, offsetY + size * 2 + 20);
+
     requestAnimationFrame(draw);
-  }, 1000 / fps);
+  }
 
+  const timeRegex = /.*T([0-9]{2}):([0-9]{2}):([0-9]{2})\..*/g;
+
+  fetch(`https://worldtimeapi.org/api/timezone/${timezone}`)
+    .then(r => r.json())
+    .then(d => {
+      currentTime = d.datetime;
+
+      [ h, m, s ] = [ 
+        parseInt(currentTime.replace(timeRegex, '$1')) % 24, 
+        parseInt(currentTime.replace(timeRegex, '$2')), 
+        parseInt(currentTime.replace(timeRegex, '$3')) 
+      ];
+
+      draw();
+    });
+
+  setInterval(() => s = (s + 1) % 60, ONE_SECOND);
+  setInterval(() => m = (m + 1) % 60, ONE_MINUTE);
+  setInterval(() => h = (h + 1) % 24, ONE_HOUR);
 }
-
-draw();
