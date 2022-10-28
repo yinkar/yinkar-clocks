bcaa39073ffdc14a046b4ff0ec8fe0a0ce357fa5
Hüseyin Karabaş <hkrabs@yandex.com>
2022-10-28 19:12:50 +0300

    Auto update

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,17 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-const { 
-        timezone = 'Europe/Istanbul', 
-        offsetX = 50, 
-        offsetY = 50, 
-        size = 100, 
-    } = defineProps({
-        timezone: String,
-        offsetX: Number,
-        offsetY: Number,
-        size: Number,
-    });
+const {
+    timezone = 'Europe/Istanbul',
+    offsetX = 50,
+    offsetY = 50,
+    size = 100,
+} = defineProps({
+    timezone: String,
+    offsetX: Number,
+    offsetY: Number,
+    size: Number,
+});
 
 const canvas = ref(null);
 
@@ -38,74 +38,89 @@ onMounted(() => {
         ctx.stroke();
     }
 
-    let [ h, m, s ] = [ 0, 0, 0 ];
+    let [h, m, s] = [0, 0, 0];
+
+    const updateTime = () => {
+        fetch(`https://worldtimeapi.org/api/timezone/${timezone}`)
+            .then(r => r.json())
+            .then(d => {
+                const currentTime = d.datetime;
+
+                [h, m, s] = [
+                    parseInt(currentTime.replace(timeRegex, '$1')) % 24,
+                    parseInt(currentTime.replace(timeRegex, '$2')),
+                    parseInt(currentTime.replace(timeRegex, '$3'))
+                ];
+
+                draw();
+            });
+    };
 
     const draw = () => {
-    ctx.clearRect(0, 0, canvas.value.width, canvas.value.height);
+        ctx.clearRect(0, 0, canvas.value.width, canvas.value.height);
 
-    ctx.font = 'bold 20px sans-serif';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.lineWidth = 6;
-    ctx.lineCap = 'round';
+        ctx.font = 'bold 20px sans-serif';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.lineCap = 'round';
 
-    for (let i = 1; i <= 12; i++) {
-        ctx.fillText(
-        i, 
-        offsetX + getX(i / 12, 1), 
-        offsetY + getY(i / 12, 1)
-        );
+        for (let i = 1; i <= 12; i++) {
+            ctx.fillText(
+                i,
+                offsetX + getX(i / 12, 1),
+                offsetY + getY(i / 12, 1)
+            );
+        }
+
+        ctx.lineWidth = 8;
+        drawHand(ctx, (h % 12) / 12, 0.4);
+        
+        ctx.lineWidth = 6;
+        drawHand(ctx, m / 60, 0.6);
+
+        ctx.lineWidth = 4;
+        drawHand(ctx, s / 60, 0.8);
+
+        ctx.beginPath();
+        ctx.arc(offsetX + size, offsetY + size, 8, 0, 2 * Math.PI);
+        ctx.fill();
+        
+
+        ctx.font = 'bold 18px monospace';
+        ctx.textBaseline = 'top';
+
+        const pad = val => val.toString().padStart(2, 0);
+
+        const digitalTime = `${pad(h)}:${pad(m)}:${pad(s)}`;
+
+        ctx.fillText(digitalTime, offsetX + size, offsetY - 35);
+
+        ctx.font = 'normal 25px monospace';
+
+        ctx.fillText(cityName, offsetX + size, offsetY + size * 2 + 12);
+
+        requestAnimationFrame(draw);
     }
 
-    drawHand(ctx, (h % 12) / 12, 0.4);
-    drawHand(ctx, m / 60, 0.6);
-    drawHand(ctx, s / 60, 0.8);
-
-    ctx.font = 'bold 18px monospace';
-    ctx.textBaseline = 'top';
-
-    const pad = val => val.toString().padStart(2, 0);
-
-    const digitalTime = `${pad(h)}:${pad(m)}:${pad(s)}`;
-
-    ctx.fillText(digitalTime, offsetX + size, offsetY - 35);
-
-    ctx.font = 'normal 25px monospace';
-
-    ctx.fillText(cityName, offsetX + size, offsetY + size * 2 + 12);
-
-    requestAnimationFrame(draw);
-    }
+    updateTime();
 
     const timeRegex = /.*T([0-9]{2}):([0-9]{2}):([0-9]{2})\..*/g;
-
-    fetch(`https://worldtimeapi.org/api/timezone/${timezone}`)
-    .then(r => r.json())
-    .then(d => {
-        const currentTime = d.datetime;
-
-        [ h, m, s ] = [ 
-        parseInt(currentTime.replace(timeRegex, '$1')) % 24, 
-        parseInt(currentTime.replace(timeRegex, '$2')), 
-        parseInt(currentTime.replace(timeRegex, '$3')) 
-        ];
-
-        draw();
-    });
 
     setInterval(() => s = (s + 1) % 60, ONE_SECOND);
     setInterval(() => m = (m + 1) % 60, ONE_MINUTE);
     setInterval(() => h = (h + 1) % 24, ONE_HOUR);
+
+    setInterval(() => updateTime(), ONE_MINUTE);
 });
 </script>
 
 <template>
-  <canvas id="canvas" width="300" height="300" ref="canvas"></canvas>
+    <canvas id="canvas" width="300" height="300" ref="canvas"></canvas>
 </template>
 
 <style scoped>
-    #canvas {
-        background-color: #fff;
-        border-radius: 50%;
-    }
+#canvas {
+    background-color: #fff;
+    border-radius: 50%;
+}
 </style>
