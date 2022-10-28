af3f0dad01685386ea4ea3cc503a539d47a6b587
Huseyin <huseyin@example.com>
2022-10-28 09:27:14 +0300

    Better look

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ const offsetX = 50;
 const offsetY = 50;
 const size = 100;
 
-const getX = (expr, factor) => Math.cos(expr * 2 * Math.PI - (Math.PI / 2)) * size * factor + size;
-const getY = (expr, factor) => Math.sin(expr * 2 * Math.PI - (Math.PI / 2)) * size * factor + size;
+const fps = 1;
+
+const getDim = (trigFunc, expr, factor) => trigFunc(
+    expr * 2 * Math.PI - (Math.PI / 2)
+  ) * size * factor + size;
+
+const getX = (expr, factor) => getDim(Math.cos, expr, factor);
+const getY = (expr, factor) => getDim(Math.sin, expr, factor);
 
 const drawHand = (ctx, expr, length) => {
     ctx.beginPath();
@@ -16,7 +22,11 @@ const drawHand = (ctx, expr, length) => {
     ctx.stroke();
 }
 
-ctx.font = '12px sans-serif';
+ctx.font = 'bold 20px sans-serif';
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.lineWidth = 6;
+ctx.lineCap = 'round';
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -29,15 +39,20 @@ const draw = () => {
     );
   }
   
-  const h = (new Date()).getHours() % 12;
-  const m = (new Date()).getMinutes();
-  const s = (new Date()).getSeconds();
+  const date = new Date();
 
-  drawHand(ctx, h / 12, 0.5);
-  drawHand(ctx, m / 60, 0.7);
-  drawHand(ctx, s / 60, 0.9);
+  const [ h, m, s ] = [ 
+    date.getHours() % 12, date.getMinutes(), date.getSeconds() 
+  ];
+
+  drawHand(ctx, h / 12, 0.4);
+  drawHand(ctx, m / 60, 0.6);
+  drawHand(ctx, s / 60, 0.8);
   
-  requestAnimationFrame(draw);
+  setTimeout(() => {
+    requestAnimationFrame(draw);
+  }, 1000 / fps);
+
 }
 
 draw();
